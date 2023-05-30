f6e56f3df02df0041705d7d036f5388ca744dfe0
jsparson1 <94084697+jsparson1@users.noreply.github.com>
2023-05-29 18:45:23 -0700

    added click events

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 camera.position.setX(-3);
 
-renderer.render(scene, camera);
 
 
 const geometry = new three.DodecahedronGeometry(5)
@@ -37,11 +36,41 @@ pointlight.position.set(10, 10, 10)
 const ambientlight = new three.AmbientLight(0xffffff);
 scene.add(pointlight);
 
+
+const raycaster = new three.Raycaster();
+const pointer = new three.Vector2();
+
+function onPointerMove(event) {
+
+  // calculate pointer position in normalized device coordinates
+  // (-1 to +1) for both components
+
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+  raycaster.setFromCamera(pointer, camera);
+  const intersects = raycaster.intersectObjects(scene.children);
+
+  for (let i = 0; i < intersects.length; i++) {
+
+    intersects[i].object.material.color.set(Math.random() * 0xffffff);
+
+  }
+
+
+}
+
+
+window.addEventListener("click", onPointerMove);
+
+
+
+
+
 function moveShape() {
   const t = document.body.getBoundingClientRect().top;
   dodecahedron.translateZ(1);
   camera.position.x = t * -0.0002;
-  console.log(camera.position.x)
 
 }
 
@@ -50,6 +79,8 @@ moveShape();
 
 function animate() {
   requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
   dodecahedron.rotation.x += 0.01;
   dodecahedron.rotation.y += 0.04;
   dodecahedron.rotation.z += 0.004;
