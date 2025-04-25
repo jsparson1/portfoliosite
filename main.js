6822fe263f51c6b79f0ca9b71d5d36a8980dc2d9
jsparson1 <94084697+jsparson1@users.noreply.github.com>
2025-04-25 15:26:10 -0400

    changed import

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import './style.css'
-import * as three from 'three';
+//import * as three from 'three';
+import * as three from 'https://unpkg.com/three@0.160.1/build/three.module.js';
+
 
 const scene = new three.Scene();
 
